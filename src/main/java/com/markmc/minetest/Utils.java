b5ea05dab74ddcc0f45ebd6c0c5765917e78c52c
Mark McLaren <Mark.McLaren@bristol.ac.uk>
2015-04-20 09:32:36 +0100

    Further JavaDocs

--- src/main/java/com/markmc/minetest/Utils.java
+++ src/main/java/com/markmc/minetest/Utils.java
@@ -14,10 +14,24 @@ import org.apache.commons.codec.binary.Hex;
  */
 public class Utils {
 
+  /**
+   * toFloat.
+   * <p>
+   * @param b byte[]
+   * <p>
+   * @return
+   */
   public static float toFloat(final byte[] b) {
     return ByteBuffer.wrap(b).getFloat();
   }
 
+  /**
+   * getByteBuffer.
+   * <p>
+   * @param hex
+   * <p>
+   * @return
+   */
   public static ByteBuffer getByteBuffer(final String hex) {
     try {
       return ByteBuffer.wrap(Hex.decodeHex(hex.toCharArray()));
@@ -27,22 +41,59 @@ public class Utils {
     return null;
   }
 
+  /**
+   * toString.
+   * <p>
+   * @param b byte[]
+   * <p>
+   * @return
+   */
   public static String toString(final byte[] b) {
     return Hex.encodeHexString(b);
   }
 
+  /**
+   * toInteger.
+   * <p>
+   * @param b byte[]
+   * <p>
+   * @return
+   */
   public static int toInteger(final byte[] b) {
     return new BigInteger(BaseEncoding.base16().encode(b), 16).intValue();
   }
 
+  /**
+   * isEqual.
+   * <p>
+   * @param b1 byte[]
+   * @param b2 byte[]
+   * <p>
+   * @return
+   */
   public static boolean isEqual(final byte[] b1, final byte[] b2) {
     return ByteBuffer.wrap(b1).equals(ByteBuffer.wrap(b2));
   }
 
+  /**
+   * makeByteArray.
+   * <p>
+   * @param v    byte[]
+   * @param size size
+   * <p>
+   * @return
+   */
   public static byte[] makeByteArray(final byte[] v, final int size) {
     return ByteBuffer.allocate(size).put(v).array();
   }
 
+  /**
+   * getByteArray.
+   * <p>
+   * @param hex
+   * <p>
+   * @return
+   */
   public static byte[] getByteArray(final String hex) {
     try {
       return Hex.decodeHex(hex.toCharArray());
@@ -51,23 +102,49 @@ public class Utils {
     return null;
   }
 
+  /**
+   * pop.
+   * <p>
+   * @param bbuff
+   * @param pop
+   * <p>
+   * @return
+   */
   public static byte[] pop(final ByteBuffer bbuff, final int pop) {
     byte[] dest = new byte[pop];
     bbuff.get(dest, 0, pop);
     return dest;
   }
 
-  // U8 = 1 byte
+  /**
+   * U8 = 1 byte.
+   * <p>
+   * @param v
+   * <p>
+   * @return
+   */
   public static byte[] U8(final int v) {
     return ByteBuffer.allocate(1).put((byte) v).array();
   }
 
-  // U16 = 2 bytes
+  /**
+   * U16 = 2 bytes.
+   * <p>
+   * @param v
+   * <p>
+   * @return
+   */
   public static byte[] U16(final int v) {
     return ByteBuffer.allocate(2).putShort((short) v).array();
   }
 
-  // U32 = 4 bytes
+  /**
+   * U32 = 4 bytes.
+   * <p>
+   * @param v
+   * <p>
+   * @return
+   */
   public static byte[] U32(final int v) {
     return ByteBuffer.allocate(4).putInt(v).array();
   }
